86772bbb38ba66608aeba92e21b30ca24d2d4702
wangzhitao <wangzhitao@co-mall.com>
2021-09-10 18:21:58 +0800

    fix: 小程序id

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,29 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/home/index',
+    'pages/member/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#333',
+    selectedColor: '#999',
+    backgroundColor: '#fff',
+    borderStyle: 'black',
+    position: 'bottom',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/member/index',
+        text: '我的'
+      }
+    ]
   }
 }
