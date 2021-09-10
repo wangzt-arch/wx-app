7e81d53385431cfcb3fb77ea86be2e3bf9582a0b
dev-wzt <1213332180@qq.com>
2021-09-11 07:49:47 +0800

    feat: weater

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import $ from "axios";
+/**
+ * 获取天气信息
+ * @param {number} cityCode
+ * @returns
+ */
+export default function getWeather(cityCode) {
+    const key = "9c49ff55128adf0fc7480c2978dbcbce",
+        city = cityCode;
+    return $.get(
+        `https://restapi.amap.com/v3/weather/weatherInfo?city=${city}&key=${key}`
+    );
+}
