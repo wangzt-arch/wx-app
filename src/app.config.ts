be9f371167f87c2837a4186efeb23381d8a9fd30
dev-wzt <1213332180@qq.com>
2021-09-11 08:11:53 +0800

    feat: TAIYANG

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,10 +3,15 @@ export default {
     'pages/home/index',
     'pages/member/index'
   ],
+  permission: {
+    'scope.userLocation': {
+      desc: '你的位置信息将用获取天气信息'
+    }
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: '我就是太阳',
     navigationBarTextStyle: 'black'
   },
   tabBar: {
@@ -18,11 +23,15 @@ export default {
     list: [
       {
         pagePath: 'pages/home/index',
-        text: '首页'
+        text: '首页',
+        iconPath: 'resource/image/home.png',
+        selectedIconPath: 'resource/image/home-select.png'
       },
       {
         pagePath: 'pages/member/index',
-        text: '我的'
+        text: '我的账户',
+        iconPath: 'resource/image/user.png',
+        selectedIconPath: 'resource/image/user-select.png'
       }
     ]
   }
