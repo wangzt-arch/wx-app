543e548445cee8963311266765247507877bcf6a
dev-wzt <1213332180@qq.com>
2021-09-12 18:54:47 +0800

    fix: TIANQI

--- src/components/ForecastWeather/index.tsx
+++ src/components/ForecastWeather/index.tsx
@@ -1,43 +1,64 @@
-import { Component } from 'react'
-import { Swiper, SwiperItem, View } from '@tarojs/components'
-import './index.scss'
+import { Component } from "react";
+import { Swiper, SwiperItem, View } from "@tarojs/components";
+import "./index.scss";
 
 interface Props {
-  weather: any,
-  style?: any
+  weather: any;
+  style?: any;
+}
+enum Week {
+  日 = 7,
+  一 = 1,
+  二 = 2,
+  三 = 3,
+  四 = 4,
+  五 = 5,
+  六 = 6,
 }
 export default class ForecastWeather extends Component<Props> {
   render() {
-    const { weather, style } = this.props
-    if (!weather) return null
+    const { weather, style } = this.props;
+    if (!weather) return null;
     return (
-      <View className='forecast-weather' style={style}>
-        <View>城市：{weather.province}-{weather.city}</View>
-        <View>城市：{weather.reporttime}</View>
-        <Swiper
-          indicatorColor='#999'
-          indicatorActiveColor='#333'
-          vertical
-          circular
-          indicatorDots
-          autoplay
-        >
-          {weather.map((item) => {
-            return <SwiperItem key={item}>item.date
-              <View>
-                <View>日期：{item.date}{item.week}</View>
-                <View>天气：{item.dayweather}</View>
-                <View>温度：{item.daytemp}℃</View>
-                <View>风力：{item.daywind}风{item.daypower}级</View>
-                <View>夜间天气：{item.nightweather}</View>
-                <View>夜间温度：{item.nighttemp}℃</View>
-                <View>夜间风力：{item.nightwind}风{item.daypower}级</View>
-              </View>
-            </SwiperItem>
+      <View className="forecast-weather" style={style}>
+        <View className="forecast-weather__item">
+          城市：{weather.province}-{weather.city}
+        </View>
+        <View className="forecast-weather__item">
+          发布时间：{weather.reporttime}
+        </View>
+        <Swiper circular indicatorDots>
+          {weather.casts.map((item) => {
+            return (
+              <SwiperItem key={item}>
+                <View>
+                  <View className="forecast-weather__item">
+                    日期：{item.date} 星期{Week[item.week]}
+                  </View>
+                  <View className="forecast-weather__item">
+                    天气：{item.dayweather}
+                  </View>
+                  <View className="forecast-weather__item">
+                    温度：{item.daytemp}℃
+                  </View>
+                  <View className="forecast-weather__item">
+                    风力：{item.daywind}风{item.daypower}级
+                  </View>
+                  <View className="forecast-weather__item">
+                    夜间天气：{item.nightweather}
+                  </View>
+                  <View className="forecast-weather__item">
+                    夜间温度：{item.nighttemp}℃
+                  </View>
+                  <View className="forecast-weather__item">
+                    夜间风力：{item.nightwind}风{item.daypower}级
+                  </View>
+                </View>
+              </SwiperItem>
+            );
           })}
         </Swiper>
-
       </View>
-    )
+    );
   }
 }
--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -1,102 +1,139 @@
-import { Component } from 'react'
-import { View, Image, Input, Button, Radio, RadioGroup, Label } from '@tarojs/components'
-import { AtFloatLayout, AtButton } from 'taro-ui'
-import Weather from '../../components/Weather'
-import ForecastWeather from '../../components/ForecastWeather'
-import getWeather, { getForecastsWeather } from '../../../src/api'
-import avatatImg from '../../resource/image/lixin.jpeg'
-import vipImg from '../../resource/image/vip.png'
-import './index.scss'
+import { Component } from "react";
+import { View, Image, Input, Radio, RadioGroup } from "@tarojs/components";
+import { AtFloatLayout, AtButton } from "taro-ui";
+import Weather from "../../components/Weather";
+import ForecastWeather from "../../components/ForecastWeather";
+import getWeather, { getForecastsWeather } from "../../../src/api";
+import avatatImg from "../../resource/image/lixin.jpeg";
+import vipImg from "../../resource/image/vip.png";
+import "./index.scss";
 
-interface Props { }
+interface Props {}
 interface State {
-  isWeatherShow: boolean,
-  weather?: {}
-  list: any
-  type?: string
-  city?: string
+  isWeatherPopupShow: boolean;
+  isWeaterShow?: boolean;
+  isForecastWeatherShow?: boolean;
+  weather?: {};
+  list: any;
+  type?: string;
+  city?: string;
 }
-export default class Index extends Component<Props, State>{
+export default class Index extends Component<Props, State> {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isWeatherShow: false,
-      city: '北京',
+      isWeatherPopupShow: false,
+      isForecastWeatherShow: false,
+      isWeaterShow: false,
+      city: "北京",
       list: [
         {
-          value: 'weather',
-          text: '实时天气',
-          checked: true
+          value: "weather",
+          text: "实时天气",
+          checked: true,
         },
         {
-          value: 'forecast',
-          text: '天气预报',
-          checked: false
+          value: "forecast",
+          text: "天气预报",
+          checked: false,
         },
-      ]
-    }
+      ],
+    };
   }
   onClickGetWeather = async () => {
-    this.setState({ isWeatherShow: true })
-  }
+    this.setState({ isWeatherPopupShow: true });
+  };
   onGetWeather = async () => {
-    const { type, city } = this.state
-    if (type === 'forecast') {
+    const { type, city } = this.state;
+    if (type === "forecast") {
       try {
-        let res = await getForecastsWeather(city)
-        this.setState({ weather: res.data.forecasts[0] })
+        let res = await getForecastsWeather(city);
+        this.setState({
+          weather: res.data.forecasts[0],
+          isForecastWeatherShow: true,
+          isWeaterShow: false,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        let res = await getWeather(city);
+        this.setState({
+          weather: res.data.lives[0],
+          isForecastWeatherShow: false,
+          isWeaterShow: true,
+        });
       } catch (error) {
         console.log(error);
       }
     }
-    else {
-      try {
-        let res = await getWeather(city)
-        this.setState({ weather: res.data.lives[0] })
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
+  };
   handleClose = () => {
-    this.setState({ isWeatherShow: false })
-  }
+    this.setState({ isWeatherPopupShow: false });
+  };
   onChangeModel = (e) => {
-    this.setState({ type: e.detail.value })
-  }
+    this.setState({ type: e.detail.value });
+  };
   onInputCity = (e) => {
-    this.setState({ city: e.detail.value })
-  }
+    this.setState({ city: e.detail.value });
+  };
   render() {
-    const { isWeatherShow, weather } = this.state
+    const { isWeatherPopupShow, weather, isWeaterShow, isForecastWeatherShow } =
+      this.state;
     return (
-      <View className='member'>
-        <View className='member__message'>
-          <View className='member__avatar'>
-            <Image className='member__image' src={avatatImg}></Image>
+      <View className="member">
+        <View className="member__message">
+          <View className="member__avatar">
+            <Image className="member__image" src={avatatImg}></Image>
           </View>
-          <View className='member__user'>
-            <View className='member__name'>user太阳</View>
-            <Image className='member__power' src={vipImg}></Image>
+          <View className="member__user">
+            <View className="member__name">user太阳</View>
+            <Image className="member__power" src={vipImg}></Image>
           </View>
-          <View className='member__weather-btn' onClick={this.onClickGetWeather}>获取天气</View>
+          <View
+            className="member__weather-btn"
+            onClick={this.onClickGetWeather}
+          >
+            获取天气
+          </View>
         </View>
-        <AtFloatLayout isOpened={isWeatherShow} title='天气' onClose={this.handleClose.bind(this)}>
-          <View className='member-props'>
-            <Input className='member-props__input' onInput={this.onInputCity} placeholder='请输入城市名称'></Input>
-            <RadioGroup onChange={this.onChangeModel} className='radio-group'>
+        <AtFloatLayout
+          isOpened={isWeatherPopupShow}
+          title="天气"
+          onClose={this.handleClose.bind(this)}
+        >
+          <View className="member-props">
+            <Input
+              className="member-props__input"
+              onInput={this.onInputCity}
+              placeholder="请输入城市名称"
+            ></Input>
+            <RadioGroup onChange={this.onChangeModel} className="radio-group">
               {this.state.list.map((item, i) => {
                 return (
-                  <Radio key={i} className='radio-group__radio' value={item.value} checked={item.checked}>{item.text}</Radio>
-                )
+                  <Radio
+                    key={i}
+                    className="radio-group__radio"
+                    value={item.value}
+                    checked={item.checked}
+                  >
+                    {item.text}
+                  </Radio>
+                );
               })}
             </RadioGroup>
           </View>
-          <AtButton className='member-props__btn' onClick={this.onGetWeather}>查询天气</AtButton>
-          {weather && <Weather weather={weather}></Weather>}
-          {/* <ForecastWeather weather={weather}></ForecastWeather> */}
+          <AtButton className="member-props__btn" onClick={this.onGetWeather}>
+            查询天气
+          </AtButton>
+          {weather && isWeaterShow && <Weather weather={weather}></Weather>}
+          {weather && isForecastWeatherShow && (
+            <ForecastWeather weather={weather}></ForecastWeather>
+          )}
         </AtFloatLayout>
+      
       </View>
-    )
+    );
   }
 }
