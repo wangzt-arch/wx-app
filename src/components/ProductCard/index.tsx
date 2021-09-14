7e74ca30fe309fe84478737dc3fecf5a51b465f3
wangzhitao <wangzhitao@co-mall.com>
2021-09-14 15:55:39 +0800

    fix: a

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -4,12 +4,18 @@ import "./index.scss";
 
 interface Props {
   imageUrl?: string;
+  describe?: string;
   title?: string;
   price?: string;
 }
 export default class ProductCard extends Component<Props> {
   render() {
-    const { imageUrl = "", title = "标题", price = "1000" } = this.props;
+    const {
+      imageUrl = "https://wztwx.oss-accelerate.aliyuncs.com/product/vegetables.webp",
+      title = "标题",
+      describe='出苗快长势好',
+      price = "1000",
+    } = this.props;
     return (
       <View className="product-card">
         <View className="product-card__image">
@@ -17,6 +23,7 @@ export default class ProductCard extends Component<Props> {
         </View>
         <View className="product-card__content">
           <View className="content__title">{title}</View>
+          <View className="content__describe">{describe}</View>
           <View className="content__price">
             ¥{price}
             <Image src="https://wztwx.oss-accelerate.aliyuncs.com/product/price-tag.png"></Image>
